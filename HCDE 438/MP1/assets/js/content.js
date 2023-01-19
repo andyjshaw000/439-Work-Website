20e4d7360113a204d159de22c5c395c059efbf81
Andy J Shaw <andyshaw@uw.edu>
2023-01-19 10:13:18 -0800

    remove commented code

--- HCDE 438/MP1/assets/js/content.js
+++ HCDE 438/MP1/assets/js/content.js
@@ -1,20 +1,5 @@
 "use strict";
 
-// window.addEventListener('load', onWindowLoaded, false);
-// function onWindowLoaded(evt) {
-//   console.log("windowloaded");
-//   let uQPandaLinks = new Set();
-//   let tempLinks = [];
-//   let images = document.querySelectorAll('img');
-//   for (let i = 0; i < images.length; i++) {
-//     chrome.runtime.sendMessage({msg: 'image', index: i}, function({data, index}){
-//       uQPandaLinks.add(data.link);
-//       tempLinks = Array.from(uQPandaLinks);
-//       images[i].src = tempLinks[Math.floor(Math.random() * tempLinks.length)];
-//     });
-//   }
-// }
-
 let uQPandaLinks = new Set();
 let tempLinks = [];
 let images = document.querySelectorAll('img');
@@ -23,5 +8,6 @@ for (let i = 0; i < images.length; i++) {
       uQPandaLinks.add(data.link);
       tempLinks = Array.from(uQPandaLinks);
       images[i].src = tempLinks[Math.floor(Math.random() * tempLinks.length)];
+      images[i].srcset = tempLinks[Math.floor(Math.random() * tempLinks.length)];
   });
 }
